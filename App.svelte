456cb1d0e2e052a4779f71b79db2930ab2bf005e
Izaak Kuipers <izaak.kuipers@gmail.com>
2023-12-10 07:14:01 +0100

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import "./css/main.css";
+
+  export let app: App;
+</script>
+
+<h1>Hello, World!</h1>
+<p>Working! App {app.metadata.name}, version {app.metadata.version}.</p>
