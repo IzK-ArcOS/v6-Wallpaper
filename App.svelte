b01b072ca84d14be3805b183f6550ced63b85720
Izaak Kuipers <izaak.kuipers@gmail.com>
2024-01-20 20:16:47 +0100

    Use `--wallpaper` instead of dedicated getter

--- App.svelte
+++ App.svelte
@@ -1,31 +1,8 @@
 <script lang="ts">
-  import { UserDataStore } from "$ts/stores/user";
-  import { getWallpaper } from "$ts/wallpaper";
   import DesktopIcons from "./Components/DesktopIcons.svelte";
   import "./css/main.css";
-
-  let url = "";
-  let previous = "";
-
-  UserDataStore.subscribe(async (v) => {
-    if (!v) return;
-
-    const wallpaper = $UserDataStore.sh.desktop.wallpaper;
-
-    if (previous == wallpaper) return;
-
-    previous = wallpaper;
-
-    const u = (await getWallpaper(wallpaper)).url;
-
-    if (u != url) url = u;
-  });
 </script>
 
-<div
-  class="wallpaper fullscreen"
-  style="background-image: url({url});"
-  data-contextmenu="shell-wallpaper"
->
+<div class="wallpaper fullscreen" data-contextmenu="shell-wallpaper">
   <DesktopIcons />
 </div>
